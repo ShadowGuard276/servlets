620c18b939b8a092cf08944a0e1934020552bee9
avi276271 <63207745+avi276271@users.noreply.github.com>
2020-06-26 13:38:40 -0400

    used req dispatcher

--- src/com/avinash/AddServlet.java
+++ src/com/avinash/AddServlet.java
@@ -3,16 +3,33 @@ package com.avinash;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet {
-	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException {
-		int i =Integer.parseInt(req.getParameter("num1"));
-		int j =Integer.parseInt(req.getParameter("num2"));
-		int k =i+j;
-		PrintWriter out =res.getWriter();
-		out.println("result is "+ k);
-}
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		Add.add(req, res);
+	}
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		Add.add(req, res);
+	}
+
+	static class Add {
+		static void add(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+			int i = Integer.parseInt(req.getParameter("num1"));
+			int j = Integer.parseInt(req.getParameter("num2"));
+			int k = i + j;
+
+			req.setAttribute("k", k);
+			RequestDispatcher rd = req.getRequestDispatcher("sq");
+			/*
+			 * PrintWriter out =res.getWriter(); out.println("result is "+ k);
+			 */
+			rd.forward(req, res);
+		}
+	}
 }
---
+++ src/com/avinash/SqServlet.java
@@ -0,0 +1,17 @@
+package com.avinash;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int k = (int) req.getAttribute("k");
+		k = k * k;
+		PrintWriter out = res.getWriter();
+		out.println(" sq of numbers is " + k);
+	}
+}
