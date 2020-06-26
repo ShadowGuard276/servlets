408157d91eb057e1ec86835bb93019969e1ec2f5
avi276271 <63207745+avi276271@users.noreply.github.com>
2020-06-26 16:33:50 -0400

    passing values using cookies method from s1 to s2

--- src/com/avinash/AddServlet.java
+++ src/com/avinash/AddServlet.java
@@ -5,9 +5,11 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
@@ -23,7 +25,11 @@ public class AddServlet extends HttpServlet {
 			int i = Integer.parseInt(req.getParameter("num1"));
 			int j = Integer.parseInt(req.getParameter("num2"));
 			int k = i + j;
-			res.sendRedirect("sq?k="+k);
+		/*	HttpSession session = req.getSession();//session 
+			session.setAttribute("k",k);*/
+			Cookie cookie =new Cookie("k",k +""); //cookies for passing values
+			res.addCookie(cookie);
+			res.sendRedirect("sq");
 			/**
 			 * req.setAttribute("k", k); 
 			 * RequestDispatcher rd = req.getRequestDispatcher("sq"); 
