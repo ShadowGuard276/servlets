f68de1aa11353abf4ba31cb89e00cd490821fb6e
avi276271 <63207745+avi276271@users.noreply.github.com>
2020-06-27 14:45:53 -0400

    used annotations insted of web Xml

--- src/com/avinash/SubServlet.java
+++ src/com/avinash/SubServlet.java
@@ -3,10 +3,11 @@ package com.avinash;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@WebServlet("/sub")
 public class SubServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		Sub.sub(req, res);
